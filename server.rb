bd40d09ff00e4d0563111e1ff46b0c4f68c65b9c
sarcocode <sarco4you@gmail.com>
2026-06-30 16:48:05 +0300

    Delete server.rb

--- server.rb
+++
@@ -1,5 +0,0 @@
-require 'webrick'
-dir = File.expand_path('..', __FILE__)
-server = WEBrick::HTTPServer.new(Port: 3000, DocumentRoot: dir, BindAddress: '127.0.0.1')
-trap('INT') { server.shutdown }
-server.start
